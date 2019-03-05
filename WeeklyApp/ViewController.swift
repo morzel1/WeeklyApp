a6144cf9e23a7144a2d7775038492111455e1be1
M <m@Ms-Mac.local>
2019-03-05 08:18:40 -0800

    Initial Commit

---
+++ WeeklyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeeklyApp
+//
+//  Created by M on 3/5/19.
+//  Copyright © 2019 rdm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
