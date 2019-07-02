c058cb1a1db45a263aae5f361d66a2f156ebcc5f
Michael <leungm425@gmail.com>
2019-07-02 11:22:03 -0400

    fixed the issue where it wouldn't reset a task after the app gets closed, still need to add the checks stated in the comments

--- WeeklyApp/AppDelegate.swift
+++ WeeklyApp/AppDelegate.swift
@@ -133,6 +133,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         }
 */
         //print("TAG enter foreground")
+        /*
         let DBHelper2 = EntryDB()
         EntryDB.ReturnFullTable(DBHelper2)()
         
@@ -232,6 +233,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                 
             }
         } //end of if check for EntryDB count
+        */
+        
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -245,6 +248,109 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
        // MainScreen().deleteTasks()
 
         //print("TAG did become active")
+        print("TAG10 BECAME ACTIVE")
+        let DBHelper2 = EntryDB()
+        EntryDB.ReturnFullTable(DBHelper2)()
+        let nilCheck = UserDefaults.standard.string(forKey: "day1")
+
+        if(nilCheck != nil){
+            let date = Date()
+            let calendar = Calendar.current
+            let hour = calendar.component(.hour, from: date)
+            let minutes = calendar.component(.minute, from: date)
+            let day = calendar.component(.weekday, from: date)
+            let seconds = calendar.component(.second, from: date)
+            
+            var compareDay = ""
+            if (day == 1){
+                compareDay = "Sunday"
+            } else if (day == 2){
+                compareDay = "Monday"
+            } else if (day == 3){
+                compareDay = "Tuesday"
+            } else if (day == 4){
+                compareDay = "Wednesday"
+            } else if (day == 5){
+                compareDay = "Thursday"
+            } else if (day == 6){
+                compareDay = "Friday"
+            } else if (day == 7){
+                compareDay = "Saturday"
+            }
+            
+            let OldDate = UserDefaults.standard.object(forKey: "date") as! Date
+            let OldDay = UserDefaults.standard.string(forKey: "day1")
+            
+            let oldCalendar = Calendar.current
+            let oldHour = oldCalendar.component(.hour, from: OldDate)
+            let oldMinutes = oldCalendar.component(.minute, from: OldDate)
+            let oldDay = oldCalendar.component(.weekday, from: OldDate)
+            let oldSeconds = oldCalendar.component(.second, from: OldDate)
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm:ss"
+            let hourString = formatter.string(from: OldDate)
+            //let savedDate = formatter.date(from: hourString)!
+            
+            //Old date comparison variables are hourString and OldDay
+            if(EntryDB.MainListStruct.MainList.count > 0){
+                for index in 0...EntryDB.MainListStruct.MainList.count-1{
+                    var dateComponents = DateComponents()
+                    
+                    var compareDay2 = 1
+                    if (EntryDB.MainListStruct.MainList[index].day == "Sunday"){
+                        compareDay2 = 1
+                    } else if (EntryDB.MainListStruct.MainList[index].day == "Monday"){
+                        compareDay2 = 2
+                    } else if (EntryDB.MainListStruct.MainList[index].day == "Tuesday"){
+                        compareDay2 = 3
+                    } else if (EntryDB.MainListStruct.MainList[index].day == "Wednesday"){
+                        compareDay2 = 4
+                    } else if (EntryDB.MainListStruct.MainList[index].day == "Thursday"){
+                        compareDay2 = 5
+                    } else if (EntryDB.MainListStruct.MainList[index].day == "Friday"){
+                        compareDay2 = 6
+                    } else if (EntryDB.MainListStruct.MainList[index].day == "Saturday"){
+                        compareDay2 = 7
+                    }
+                    
+                    let stringDate = EntryDB.MainListStruct.MainList[index].time
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "hh:mm a"
+                    let date2 = dateFormatter.date(from: stringDate)!
+                    let calendar2 = Calendar.current
+                    let hour2 = calendar2.component(.hour, from: date2)
+                    let minute2 = calendar2.component(.minute, from: date2)
+                    let second2 = calendar2.component(.second, from: date2)
+                    
+                    dateComponents.day = compareDay2
+                    dateComponents.hour = hour2
+                    dateComponents.minute = minute2
+                    
+                    //(oldHour): \(oldMinutes): \(oldSeconds) -> \(oldDay) is the old variables
+                    //hour, minutes, seconds, compareDay is string, day is int
+                    //hour2, minute2, compareDay2 is the saved date from the DB
+                    print("TAG10 Tasklist \(EntryDB.MainListStruct.MainList)")
+                    print("TAG10 Oldlist \(oldHour): \(oldMinutes): \(oldSeconds) -> Day \(oldDay)")
+                    print("Tag10 CurrentList \(hour): \(minutes): \(seconds) -> Day \(day)")
+                    print("TAG10 DatabaseList \(hour2): \(minute2): \(second2) -> DAY \(compareDay2)")
+                    
+                    //checks if the the app was reopened in the same day of the week then checks if database time is within old and new
+                    if(oldDay == compareDay2) && (compareDay2 == day){
+                        if(oldHour <= hour2 && hour2 <= hour) && (oldMinutes <= minute2 && minute2 <= minutes) && (EntryDB.MainListStruct.MainList[index].status == "1"){
+                            print("TAG12 BING BING TASK is today")
+                            MainScreen().DBHelper.updateTableCheckboxPressed(arg: EntryDB.MainListStruct.MainList[index].id-1)
+                        }
+                    }
+                    
+                    //use cases i need to cover: transition from saturday to sunday (7 to 1 in day int)
+                    //check for within a week (1 to 7 in day int)
+                    //check for midnight transition (23:59:59 to 00:00:00) *possibly use change in the day as the factor
+                    //
+                    
+                }
+            } //end of if check for EntryDB count
+        }
 
     }
 
