fe6bd9ca4e66cf2b8cedf3bafef42c95e52e7c57
Michael <leungm425@gmail.com>
2019-07-25 14:20:28 -0400

    Got adMob integration working and adjusted the UI to compensate. The only thing left is to revise the UI of the second screen. Possible idea is setting default title to the pick views so that when they type something and click done, it replaces the title words, cleaner UI?

--- WeeklyApp/MainScreen.swift
+++ WeeklyApp/MainScreen.swift
@@ -9,8 +9,10 @@
 import UIKit
 import SQLite3
 import UserNotifications
+import GoogleMobileAds
+import AudioToolbox
 
-class MainScreen: UIViewController, UITableViewDelegate, UITableViewDataSource, UNUserNotificationCenterDelegate  {
+class MainScreen: UIViewController, UITableViewDelegate, UITableViewDataSource, UNUserNotificationCenterDelegate, GADBannerViewDelegate{
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -348,6 +350,13 @@ class MainScreen: UIViewController, UITableViewDelegate, UITableViewDataSource, 
         MainTable.reloadData()
     }
     
+    var adMobBannerView = GADBannerView()
+    
+    //real banner
+    //let ADMOB_BANNER_UNIT_ID = "ca-app-pub-2287959670984169/7335978702"
+
+    //test banner
+    let ADMOB_BANNER_UNIT_ID = "ca-app-pub-3940256099942544/2934735716"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -357,6 +366,8 @@ class MainScreen: UIViewController, UITableViewDelegate, UITableViewDataSource, 
         //add check here
         
         self.MainTable.reloadData()
+        
+        initAdMobBanner()
         
         //MainTable.rowHeight = UITableView.automaticDimension
         //MainTable.estimatedRowHeight = 100
@@ -489,6 +500,56 @@ class MainScreen: UIViewController, UITableViewDelegate, UITableViewDataSource, 
         
         //displaying the ios local notification when app is in foreground
         completionHandler([.alert, .badge, .sound])
+    }
+    
+    
+    // MARK: -  ADMOB BANNER
+    func initAdMobBanner() {
+        
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            // iPhone
+            adMobBannerView.adSize =  GADAdSizeFromCGSize(CGSize(width: 320, height: 50))
+            adMobBannerView.frame = CGRect(x: 0, y: view.frame.size.height, width: 320, height: 50)
+        } else  {
+            // iPad
+            adMobBannerView.adSize =  GADAdSizeFromCGSize(CGSize(width: 468, height: 60))
+            adMobBannerView.frame = CGRect(x: 0, y: view.frame.size.height, width: 468, height: 60)
+        }
+        
+        adMobBannerView.adUnitID = ADMOB_BANNER_UNIT_ID
+        adMobBannerView.rootViewController = self
+        adMobBannerView.delegate = self
+        view.addSubview(adMobBannerView)
+        
+        let request = GADRequest()
+        adMobBannerView.load(request)
+    }
+    
+    
+    // Hide the banner
+    func hideBanner(_ banner: UIView) {
+        UIView.beginAnimations("hideBanner", context: nil)
+        banner.frame = CGRect(x: view.frame.size.width/2 - banner.frame.size.width/2, y: view.frame.size.height - banner.frame.size.height, width: banner.frame.size.width, height: banner.frame.size.height)
+        UIView.commitAnimations()
+        banner.isHidden = true
+    }
+    
+    // Show the banner
+    func showBanner(_ banner: UIView) {
+        UIView.beginAnimations("showBanner", context: nil)
+        banner.frame = CGRect(x: view.frame.size.width/2 - banner.frame.size.width/2, y: view.frame.size.height - banner.frame.size.height, width: banner.frame.size.width, height: banner.frame.size.height)
+        UIView.commitAnimations()
+        banner.isHidden = false
+    }
+    
+    // AdMob banner available
+    func adViewDidReceiveAd(_ view: GADBannerView!) {
+        showBanner(adMobBannerView)
+    }
+    
+    // NO AdMob banner available
+    func adView(_ view: GADBannerView!, didFailToReceiveAdWithError error: GADRequestError!) {
+        hideBanner(adMobBannerView)
     }
 
 }
--- WeeklyApp/TimeSet.swift
+++ WeeklyApp/TimeSet.swift
@@ -9,8 +9,10 @@
 import UIKit
 import Foundation
 import SQLite3
+import GoogleMobileAds
+import AudioToolbox
 
-class TimeSet: UIViewController, UITextFieldDelegate{
+class TimeSet: UIViewController, UITextFieldDelegate, GADBannerViewDelegate{
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
@@ -99,8 +101,18 @@ class TimeSet: UIViewController, UITextFieldDelegate{
     }
     //End of name picker
     
+    var adMobBannerView = GADBannerView()
+    
+    //real banner
+    //let ADMOB_BANNER_UNIT_ID = "ca-app-pub-2287959670984169/7335978702"
+    
+    //test banner
+    let ADMOB_BANNER_UNIT_ID = "ca-app-pub-3940256099942544/2934735716"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initAdMobBanner()
+
         //initializes the time picker
         createDatePicker()
         createNamePicker()
@@ -139,6 +151,55 @@ class TimeSet: UIViewController, UITextFieldDelegate{
         // Do any additional setup after loading the view, typically from a nib.
         
 
+    }
+    
+    // MARK: -  ADMOB BANNER
+    func initAdMobBanner() {
+        
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            // iPhone
+            adMobBannerView.adSize =  GADAdSizeFromCGSize(CGSize(width: 320, height: 50))
+            adMobBannerView.frame = CGRect(x: 0, y: view.frame.size.height, width: 320, height: 50)
+        } else  {
+            // iPad
+            adMobBannerView.adSize =  GADAdSizeFromCGSize(CGSize(width: 468, height: 60))
+            adMobBannerView.frame = CGRect(x: 0, y: view.frame.size.height, width: 468, height: 60)
+        }
+        
+        adMobBannerView.adUnitID = ADMOB_BANNER_UNIT_ID
+        adMobBannerView.rootViewController = self
+        adMobBannerView.delegate = self
+        view.addSubview(adMobBannerView)
+        
+        let request = GADRequest()
+        adMobBannerView.load(request)
+    }
+    
+    
+    // Hide the banner
+    func hideBanner(_ banner: UIView) {
+        UIView.beginAnimations("hideBanner", context: nil)
+        banner.frame = CGRect(x: view.frame.size.width/2 - banner.frame.size.width/2, y: view.frame.size.height - banner.frame.size.height, width: banner.frame.size.width, height: banner.frame.size.height)
+        UIView.commitAnimations()
+        banner.isHidden = true
+    }
+    
+    // Show the banner
+    func showBanner(_ banner: UIView) {
+        UIView.beginAnimations("showBanner", context: nil)
+        banner.frame = CGRect(x: view.frame.size.width/2 - banner.frame.size.width/2, y: view.frame.size.height - banner.frame.size.height, width: banner.frame.size.width, height: banner.frame.size.height)
+        UIView.commitAnimations()
+        banner.isHidden = false
+    }
+    
+    // AdMob banner available
+    func adViewDidReceiveAd(_ view: GADBannerView!) {
+        showBanner(adMobBannerView)
+    }
+    
+    // NO AdMob banner available
+    func adView(_ view: GADBannerView!, didFailToReceiveAdWithError error: GADRequestError!) {
+        hideBanner(adMobBannerView)
     }
     
 
@@ -286,6 +347,9 @@ class dropDownView: UIView, UITableViewDelegate, UITableViewDataSource{
         //print(dropDownOptions[indexPath.row])
         self.tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    
+    
     
 }
 // end of drop down button code
